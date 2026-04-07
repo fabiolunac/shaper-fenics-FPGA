782e296d470900c17d60be954e239c76889927c5
Fabio Luna <fabioluna20@gmail.com>
2026-04-07 15:47:02 +0200

    update file

--- pzc_shift.py
+++ pzc_shift.py
@@ -13,29 +13,25 @@ def twos_complement_to_int(bits):
 
 
 def main():
-    arquivo = Path(__file__).with_name("pzc_comparison_sh9.txt")
-
-    # N = 10000
+    arquivo = Path(__file__).with_name("pzc_comparison.txt")
 
     with arquivo.open("r", encoding="utf-8") as f:
         linhas = [linha.strip().split() for linha in f if linha.strip()]
 
-    pzc_out, pzc12_out, adc_out = [], [], []
+    pzc_out, adc_out = [], [], []
 
     for linha in linhas[1:]:
         if len(linha) != 3:
             continue
 
-        pzc, pzc12, adc = linha
+        pzc, adc = linha
         pzc_out.append(twos_complement_to_int(pzc))
-        pzc12_out.append(int(pzc12,2))
         adc_out.append(int(adc,2))
 
     pzc_out = np.array(pzc_out)
-    pzc12_out = np.array(pzc12_out)
     adc_out = np.array(adc_out)
 
-    erro = adc_out - pzc12_out
+    erro = adc_out - pzc_out
 
     if False:
         fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
@@ -64,8 +60,8 @@ def main():
         ax1.set_title("ADC output")
         ax1.set_ylabel("Valor")
 
-        ax3.plot(pzc12_out)
-        ax3.set_title("PZC 12b output")
+        ax3.plot(pzc_out)
+        ax3.set_title("PZC 13b output")
         ax3.set_xlabel("Amostra")
         ax3.set_ylabel("Valor")
 
@@ -76,7 +72,7 @@ def main():
         fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
         ax1.plot(adc_out, label='Shaper Output')
-        ax1.plot(pzc12_out, label='PZC 12 bits')
+        ax1.plot(pzc_out, label='PZC 13 bits')
         ax1.legend()
         ax1.set_title('Signal Comparison')
 
